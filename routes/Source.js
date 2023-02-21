fc83a15819d36ae75853b6b4cc7af296946094c2
Ryan Tan <ryantsk.git@gmail.com>
2023-02-21 16:27:33 +0000

    added minor query - need to add try/catch loop

--- routes/Source.js
+++ routes/Source.js
@@ -3,8 +3,10 @@ const Quote = require("../model/Quotes")
 const router = express.Router()
 
 router.get("/", async (req, res)=> {
-  const quoteData = await Quote.findOne()
-  res.send(quoteData)
+  if (Object.keys(req.query).length ===0){
+    await Quote.findOne().then(data => {res.send(data)})
+  }
+  Quote.findOne(req.query).then(data=> {res.send(data)})
 })
 
 module.exports = router
